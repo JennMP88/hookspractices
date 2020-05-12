f736d100be7518f68f5d38e2ede34856c95514d2
Jennifer Perez <jenniferperez@pursuit.org>
2020-05-12 18:31:24 -0400

    slicing a  paragraph

--- src/App.js
+++ src/App.js
@@ -9,54 +9,54 @@ class App extends Component {
       { name: "Manu", age: 29 },
       { name: "Stephanie", age: 26 },
     ],
-    showPersons:false
+    showPersons: false,
   };
-  switchNameHandler = (newName) => {
-    // console.log('Was clicked!')
+
+
+  nameChangedHandler = (event) => {
     this.setState({
       persons: [
-        { name: newName, age: 28 },
-        { name: "Manu", age: 30 },
+        { name: "Max", age: 28 },
+        { name: event.target.value, age: 30 },
         { name: "Stephanie", age: 26 },
       ],
     });
   };
 
-  nameChangedHandler=(event)=>{
-    this.setState({
-      persons: [
-        { name: 'Max', age: 28 },
-        { name: event.target.value, age: 30 },
-        { name: "Stephanie", age: 26 },
-      ],
-    })
+  deletePersonHandler=(personIndex)=>{
+   const persons=[...this.state.persons];
+   persons.splice(personIndex,1)
+   this.setState({persons:persons}) //set to new updated person
   }
-  togglePeronsHandler=()=>{
-    const doesShow=this.state.showPersons;
-    this.setState({showPersons: !doesShow})
-  }
+
+
+  togglePeronsHandler = () => {
+    const doesShow = this.state.showPersons;
+    this.setState({ showPersons: !doesShow });
+  };
 
   render() {
-    const style={
-      backgroundColor: 'white',
-      font:'inherit',
-      border:'1px solid blue',
-      padding:'8px',
-      cursor:'pointer'
-    }
+    const style = {
+      backgroundColor: "white",
+      font: "inherit",
+      border: "1px solid blue",
+      padding: "8px",
+      cursor: "pointer",
+    };
 
-    let persons=null;
-    if(this.state.showPersons){
-      persons=(
-
-        <div >  
-          {this.state.persons.map(person=>{
-            return <Person name={person.name} 
-            age={person.age}/>
+    let persons = null;
+    if (this.state.showPersons) {
+      persons = (
+        <div>
+          {this.state.persons.map((person,index) => {
+            return <Person 
+            click={()=> {this.deletePersonHandler(index)}}
+            // click={this.deletePersonHandler.bind(this,index)}
+            name={person.name} 
+            age={person.age} />;
           })}
-   
-      </div>
-      )
+        </div>
+      );
     }
     return (
       <div className="App">
@@ -64,13 +64,11 @@ class App extends Component {
         <p>This is a really working!</p>
         {/* returns a function call, arrow function. not executed immediately.pass an anonymous function, then gets executed when function on click is done.
         takes a event object technically as an obj. technically returns a function call */}
-        <button 
-        style={style}
-        onClick={()=>this.togglePeronsHandler()}>Toggle Persons</button>
-       
+        <button style={style} onClick={() => this.togglePeronsHandler()}>
+          Toggle Persons
+        </button>
 
         {persons}
-    
       </div>
     );
   }
