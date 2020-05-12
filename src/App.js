b055cd42f140ded63c8a8a728551c4fb6b14059c
Jennifer Perez <jenniferperez@pursuit.org>
2020-05-12 17:45:10 -0400

    made content more dynamic by mapping through my state items

--- src/App.js
+++ src/App.js
@@ -48,23 +48,13 @@ class App extends Component {
     let persons=null;
     if(this.state.showPersons){
       persons=(
+
         <div >  
-        <Person
-          name={this.state.persons[0].name}
-          age={this.state.persons[0].age}
-        />
-        <Person
-          name={this.state.persons[1].name}
-          age={this.state.persons[1].age}
-          // alterntive method from arrow is bind
-          click={this.switchNameHandler.bind(this,"Jenniiiiiifoforo" )}
-          changed={this.nameChangedHandler}> My Hobbies: Racing
-          </Person>
-      
-        <Person
-          name={this.state.persons[2].name}
-          age={this.state.persons[2].age}
-        />
+          {this.state.persons.map(person=>{
+            return <Person name={person.name} 
+            age={person.age}/>
+          })}
+   
       </div>
       )
     }
